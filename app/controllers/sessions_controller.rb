36e13ef9f750500491293c2d756202e9136df7a0
Abram Bailey <funkdified@gmail.com>
2012-01-27 16:31:11 +1300

    done with signin

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  
+  def new
+  	@title = "Sign in"
+  end
+   
+	def create
+		#hits self.authenticate in models/user.rb
+		user = User.authenticate(params[:session][:email],
+		                         params[:session][:password])
+		if user.nil?
+		  # Create an error message and re-render the signin form.
+		  flash.now[:error] = "Invalid email/password combination."
+		  @title = "Sign in"
+		  render :new
+		else
+		flash[:success] = "Welcome back to Good Sounds #{user.name}!"
+		
+		# Hits sign_in in helpers/sessions_helper.rb
+		# with user object returned by authenticate in user.rb 
+		sign_in(user)
+		
+		# Sign the user in and redirect to the user's show page.
+		redirect_to user
+		end
+	end
+	
+	def destroy
+	
+		#in session_helper.rb
+		sign_out
+		
+		redirect_to root_path
+	end
+
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,43 @@
+module SessionsHelper
+
+  def sign_in(user)
+  
+  	#Creates secure token based on userid and salt
+    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    
+    #Creates current_user, accessible to controllers (since sessions_helper was required in application_controller.rb)and views
+    #which will allow constructions such as "current_user.name" and "redirect_to current user"
+    self.current_user = user
+  end
+  
+  def current_user=(user)
+  	#Just assigns user to @current_user
+    @current_user = user
+  end
+  
+   def current_user
+    @current_user ||= user_from_remember_token #calls user_from_remem private method (below)
+    #assigns @current user based on remember token only when @current_user has not already been defined (by remember token) -- assigns first time and not again.
+  end
+  
+  #You are signed in if the current user is not nil.
+  def signed_in?
+  	!current_user.nil?
+	end
+	
+	def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+  
+  private
+  
+    def user_from_remember_token
+      User.authenticate_with_salt(*remember_token) 
+      # '*' allows array to pass in as multiple arguments (taken from array values) rather than as a single object (array)
+    end
+    
+    def remember_token
+      cookies.signed[:remember_token] || [nil, nil]
+    end
+end
