49cc3cf1e05db5e2d09374b620d35deb7138652a
Abram Bailey <funkdified@gmail.com>
2012-01-29 12:51:20 +1300

    done with ch10.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,6 +30,21 @@ module SessionsHelper
     self.current_user = nil
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def deny_access
+  	store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+  
+  # this uses location stored from deny_access above to forward once user has signed in
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
   private
   
     def user_from_remember_token
@@ -39,5 +54,13 @@ module SessionsHelper
     
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+    
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end
